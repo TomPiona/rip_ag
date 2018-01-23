36fe1fa4e4de7fc21e42ef7f9b064208de56e183
Ronald Walker <ronald.walker@berkeley.edu>
2018-01-23 14:34:11 -0800

    deciding on test config

---
+++ tests/example_test.py
@@ -0,0 +1,12 @@
+# commented lines are added in main file
+# __score__ = 0
+
+__out_of__ = 2
+
+if variable_to_check == 3:
+	__score__ += 1
+if func_to_check(4) == 12:
+	__score__ += 1
+
+# __total__ += __out_of__
+# __points__ += __score__
