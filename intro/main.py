326081de4b1b0f0a5b48fe7da12b95a0e90b900a
Ronald Walker <ronald.walker@berkeley.edu>
2018-01-25 00:30:51 -0800

    turns out the code was all excluded, need to figure out error handling for student code

--- intro/main.py
+++ intro/main.py
@@ -4,10 +4,14 @@ import re
 import os
 import tests.configs as cfg
 
+# extra
+from halo import Halo
+
 # TODO: 
 # need to catch failing notebooks
 # needs more testing against notebooks w/ known scores
 
+@Halo(text='reading nb', spinner='dots')
 def __read_nb__(filename):
     """returns the notebook as a dictionary"""
     with open(filename) as f:
@@ -49,7 +53,6 @@ def __create_tests_(test_text):
     return ''.join(test_text)
 
 def __run_tests__(code, tests):
-    
     # loading animation
     # "running for ___"
     exec(code + '\n' + tests)
@@ -65,12 +68,19 @@ def __pls_sanitize__(list_of_code):
     # keep track of files w/ errors
     # approved imports
     """s"""
-    some_clean = [line for line in list_of_code if not line.startswith('_ =') and \
+    some_clean = [line.replace('\n', '') for line in list_of_code if not line.startswith('_ =') and \
         not 'print' in line and not line.startswith('ok.') and \
         not line.startswith('from client.api.notebook') and \
         not line.startswith('plt.') and not line.startswith('\tplt.') and\
-        not line.startswith('!')]
-    return [line + '\n\t#for functions' for line in some_clean if line.startswith('def') and not line.endswith('\n')]
+        not line.startswith('!') and not line.startswith('%') and\
+        not line.startswith('ok =') and not line.startswith('import matplot')]
+    insertion_adjustment = 0
+    for i in range(len(some_clean)):
+        line = some_clean[i + insertion_adjustment]
+        if line.startswith('for ') or line.startswith('def '):
+            some_clean.insert(i + insertion_adjustment + 1, '    """trouble"""')
+
+    return some_clean
 
 ###################################
 ###################################
@@ -108,36 +118,34 @@ def run_one(filepath, tests):
     # getting numerical score on code questions
     cleaned_code = __pls_sanitize__(c[0])
     score = __run_tests__('\n'.join(cleaned_code), tests)
+    print(score)
 
-    if len(c[1]) != cfg.num_FR:
-        print("number of FR answers does not align for {}".format(user_id))
+    # if len(c[1]) != cfg.num_FR:
+    #     print("number of FR answers does not align for {}".format(user_id))
 
 
     return user_id, score, c[1]
 
-
-def run_all(filename):
+@Halo(text='running tests', spinner='dots')
+def run_all():
 
     # create csv outline
     # user_id, code_score, FR1, FR2, ..., FRn
 
+    test_text = __get_tests__()
+    ts = __create_tests_(test_text)
+
     for filename in get_notebook_names():
-        line = [filename]
+        l = run_one(filename, ts)
+        #print(l)
 
 
     # write csv
 
-# if __name__ == '__main__':
-#     print('loading tests...\n')
-#     test_text = __get_tests__()
-#     ts = __create_tests_(test_text)
+if __name__ == '__main__':
+    run_all()
 
-#     for each in get_notebook_names():
-#         print(each)
+# test_text = __get_tests__()
+# ts = __create_tests_(test_text)
 
-#     c = run_one('./chench@berkeley.edu/R60P1q/intro.ipynb')
-
-test_text = __get_tests__()
-ts = __create_tests_(test_text)
-
-c = run_one('./chench@berkeley.edu/R60P1q/intro.ipynb', ts)
+# c = run_one('./chench@berkeley.edu/R60P1q/intro.ipynb', ts)
--- intro/tests/test1.py
+++ intro/tests/test1.py
@@ -1,18 +1,18 @@
 __out_of__ = 3
 
 try:
-	if x == 10.5:
-		__score__ += 1
-except: 
-  pass
+    if x == 10.5:
+        __score__ += 1
+except Exception as ex:
+    pass
 try:
-	if y == 7.2:
-		__score__ += 1
+    if y == 7.2:
+        __score__ += 1
 except: 
-  pass
+    pass
 try:
-	import numpy as np
-	if np.isclose(combo, 75.6):
-		__score__ += 1
+    import numpy as np
+    if np.isclose(combo, 75.6):
+        __score__ += 1
 except: 
-  pass
+    pass
